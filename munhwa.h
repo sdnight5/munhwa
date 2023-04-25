6cb4b18ca7f67e03d52bed008281e3f3b8779b6d
Dina S <110544189+sdnight5@users.noreply.github.com>
2023-04-25 23:21:44 +0900

    Add files via upload

---
+++ munhwa.h
@@ -0,0 +1,111 @@
+/*
+    Template made for North Korean programming!
+    문화어로 콤푸타 사용하고 싶었던 린민들을 위해!
+    버젼: Beta ㅣ 연결고리: github.com/sdnight5/munhwa
+*/
+
+// 머리말 및 별명미리붙이기
+#include <bits/stdc++.h>
+using namespace std;
+
+// 린민확인하기
+#define 린민들여보내기(x) cin >> x
+#define 아오지탄광보내기(x) cout << x
+
+// 일꾼 관련 함수
+#define 일꾼추가하기 ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define 일꾼수송준비하기 cout << fixed; cout.precision(20)
+
+// 기본 구문
+#define 만약 if
+#define 아니라면 else
+#define 반복 for
+#define 조건을만족하는동안 while
+#define 형벌 break
+#define 하위형벌 continue
+#define 그리고 &&
+#define 또는 ||
+
+// 전구 연산
+#define 저런 !
+#define 역모임 ^
+#define 교모임 &
+#define 합모임 |
+#define 왼쪽밀기 <<
+#define 오른쪽밀기 >>
+
+// 자료형
+#define 정수 int
+#define 영일 bool
+#define 문자 char
+#define 소수 double
+#define 긴실수 long double
+#define 문자열 string
+#define 긴정수 long long
+#define 핵미사일 auto
+#define 처형안해도되는 void
+#define 행군짝 pair
+#define 정한 const
+#define 행군앞 first
+#define 행군뒤 second
+
+// 기본 도서관
+#define 뱃살같은행군 vector
+#define 모임 set
+#define 린민모임 multiset
+#define 린민사전 unordered_set
+#define 사전 map
+#define 여러사전 multimap
+#define 린민여러사전 unordered_map
+#define 수령님부터시작하는 priority_queue
+#define 미사일쌓기 stack
+#define 미사일발포대기열 queue
+#define 미사일발포여러방면대기열 deque
+
+#define 수령님이드신다 push
+#define 수령님이토한다 pop
+#define 행군앞쪽추가 push_front
+#define 행군뒤쪽추가 push_back
+#define 행군앞쪽제거 pop_front
+#define 행군뒤쪽제거 pop_back
+
+#define 수령님배고프시다 empty
+#define 크기 size
+#define 머리 top
+#define 배 front
+#define 등 back
+
+#define 미사일장전 insert
+#define 미사일발사 erase
+#define 미사일확인 find
+#define 미사일개수확인 count
+
+#define 선전포고 begin
+#define 항복 end
+#define 적군선전포고 rbegin
+#define 적군항복 rend
+
+#define 수령님뱃살 max
+#define 린민뱃살 min
+#define 행군정렬 sort
+#define 수령님지능만큼 lower_bound
+#define 수령님뱃살만큼 upper_bound
+
+#define 수령님의뱃살같은 greater
+
+#define 여러곱셈 pow
+ 
+// 자료값
+#define 수령님의말 true
+#define 남한의말 false
+#define 천민수 NULL
+#define 여백 " "
+#define 밑으로 "\n"
+ 
+// 기타
+#define 공개처형식 return 0
+#define 아오지탄광행 return
+#define 기본함수 main
+
+// 아오지탄관행 단어
+#define 수령님지능 0
